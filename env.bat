99c85009676372e6ad0c360742c4beee962b8ae5
Jorge Mendez <lic.jorgemendez@gmail.com>
2023-03-03 06:42:59 -0500

    Inicio del proyecto, stack React y Laravel

---
+++ env.bat
@@ -0,0 +1,6 @@
+C:
+start cmd /K "cd C:\Users\progr\proyectos\InPayU\backend"
+C:
+start cmd /K "cd C:\Users\progr\proyectos\InPayU\backend && php artisan serve"
+C:
+start cmd /K "cd C:\Users\progr\proyectos\InPayU\pwa && npm start"
